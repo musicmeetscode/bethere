e4e232788d7699603a0927fdbcdef9bcaab5bf7a
Nahabwe Edwin <nahabwe.edwin12@gmail.com>
2025-10-15 16:54:58 +0300

    Finalised i'll be there

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,154 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import html2canvas from 'html2canvas-pro'
+import { Calendar, Clock, Download, ImageIcon, LocateIcon } from 'lucide-react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setname] = useState()
+  const [profession, setprofession] = useState()
+  const divRef = useRef(null)
+
+  const [profileImage, setProfileImage] = useState('/img/sample.webp')
+  const fileInputRef = useRef(null)
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      // When the reader has finished loading the file
+      reader.onload = () => {
+        // The result is the Base64 data URL
+        setProfileImage(reader.result);
+      };
+
+      // Read the file as a Data URL (Base64 encoded string)
+      reader.readAsDataURL(file);
+    }
+  };
+  // 4. Function to trigger the file input click
+  const triggerFileUpload = () => {
+    fileInputRef.current.click();
+  }
+
+  const download = async () => {
+    if (divRef.current) {
+      const canvas = await html2canvas(divRef.current)
+      const image = canvas.toDataURL('image/png')
+      const link = document.createElement('a')
+      link.href = image;
+      link.download = name + "-devfest.png"
+      document.body.appendChild(link)
+      link.click()
+      document.removeChild(link)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className=" inset-0">
+      {/* Navigation */}
+      <div className="flex w-full fixed top-0 left-0 right-0 p-4 z-[999999999] shadow justify-center items-center text-xl hover:cursor-pointer">
+        <nav className="w-full max-w-7xl items-center flex justify-between font-semibold ">
+          <div className="flex items-center gap-2">
+            <img src="/public/img/gdg-cloud.png" className='h-18 object-contain' alt="" />
+
+          </div>
+
+          <div className="flex items-center justify-between gap-5 text-sm max-w-sm ">
+            <div className="hidden md:block hover:cursor-pointer hover:font-bold underline underline-offset-4 text-green-600">Create my dp</div>
+            <a target='_blank' href='https://gdg.community.dev/events/details/google-gdg-cloud-mbarara-presents-devfest-mbarara-2025/' className="hidden md:block  hover:cursor-pointer hover:font-bold ">About Devfest</a>
+            <a target='_blank' href='https://gdg.community.dev/events/details/google-gdg-cloud-mbarara-presents-devfest-mbarara-2025/' className="hover:cursor-pointer hover:font-bold bg-green-600 shadow px-5 py-3 text-white">RSVP</a>
+          </div>
+        </nav>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex  z-20 justify-center items-center mt-24">
+        <div className=" h-sceen mt-24 mx-12 flex flex-col lg:flex-row items-center gap-2 lg:gap-24 justify-center max-w-7xl">
+          <div className=" flex flex-col gap  ">
+            <div className="text-4xl font-bold text-blue-500 mb-2">Devfest Mbarara, 2025</div>
+            <div className="text-5xl font-bold tracking-wide">Get your <span className='text-green-600' >devfest DP</span> </div>
+            <div className="text-lg mt-2">Signed up for devfest? get your customised devfest dp</div>
+            <div className="flex flex-col md:flex-row  gap-4 items-center  mt-10 md:mt-8">
+              <div className="flex flex-col">
+                <label htmlFor="">Your profession</label>
+                <input type="text" className='w-full rounded outline-1 outline-gray-400  px-4 py-4' value={profession} onChange={(e) => setprofession(e.target.value)} placeholder='Your profession' />
+              </div>
+              <div className="flex flex-col">
+
+                <label htmlFor="">Your name</label>
+                <div className="m shaow-lg flex items-center border-1 border-gray-400 px-1 rounded ">
+
+                  <input type="text" className='w-full ml-2 outline-none focus:outline-none' value={name} onChange={(e) => setname(e.target.value)} placeholder='Your name' />
+                  <div className="bg-green-600  hover:bg-orange-400 rounded text-white hover:cursor-pointer px-4 py-3 my-1">
+                    Submit
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex gap-4 items-center my-4 ">
+              <input type="file" ref={fileInputRef} onChange={handleImageUpload} name="file-input" id="fileinput" className='hidden' accept='image/*' />
+              <button onClick={triggerFileUpload} className="my-10 bg-orange-500 flex py-4 px-4 w-full shadow text-white rounded hover:cursor-pointer "> <ImageIcon className="mr-4"/> Upload photo</button>
+              <button onClick={() => download()} className={`my-10 ${name && (profileImage!="/img/sample.webp")?'bg-green-500 text-white':'bg-gray-400 text-gray-300'} flex py-4 px-4 w-full shadow rounded hover:cursor-pointer `}>  <Download className="mr-4"/> Download dp</button>
+            </div>
+          </div>
+          <div ref={divRef} className="hidden size-[600px] border-1   md:flex flex-col p-4 px-8">
+            <div className="flex items-center  py-4">
+              <img className='w-1/2 object-contain h-10' src="/img/gdg-cloud.png" alt="" />
+              <img className='w-1/2 object-contain h-10' src="/img/wtm.jpeg" alt="" />
+
+            </div>
+            <hr className='h-0.5 w-full bg-gray-200' />
+            <div className=" h-[74%] items-center  flex ">
+
+              {/* Person photo */}
+
+              <div className="flex flex-col w-1/2  justify-start items-center ">
+                <div className="text-center w-full ">I will be attending</div>
+                <img src="/public/img/devfest.png" className='size-40 -mt-10 object-contain' alt="" />
+                {/* <div className="font-semibold w-full text-2xl">Devfest Mbarara</div>
+              <div className="font-bold w-full text-3xl">2025</div> */}
+                <div className="flex flex-col gap-2 ">
+
+                  <div className="flex items-center  gap-2">
+                    <LocateIcon /> MUST, Kihumuro
+                  </div> <div className="flex items-center gap-2">
+                    <Calendar /> 15th November, 20205
+                  </div> <div className="flex items-center gap-2">
+                    <Clock />  9AM-5PM
+                  </div>
+                </div>
+
+                {/* <div className="bg-green-500 flex items-end justify-center z-50 border-1 rounded-2xl h-40 mt-12 w-4/5">
+                <div className="h-32 bg-white w-3/4 border rounded-t-2xl ">
+                  <div className="size-4 rounded-full border font-semibold text-4xl mx-auto -mt-1 bg-red-600"></div>
+                  <div className="text-2xl text-center text-wrap">
+                    Nov 15th <br /> 2025 <br />
+                    <span className='text-sm'>MUST, Kihumuro Campus</span>
+
+                  </div>
+
+
+                </div>
+              </div> */}
+              </div>
+              <div className="w-1/2 h-3/5  flex flex-col  rounded-t-lg">
+                <img src={profileImage} className=' w-full border-1 rounded-t-xl mb-1 h-full object-cover ' alt="" />
+                <div className="text-2xl text-center font-semibold">{name ?? "Your name"}</div>
+                <div className="text-lg -mt-2 font-light text-center">{profession ?? "Your profession"}</div>
+
+              </div>
+            </div>
+            {/* Bottom cards */}
+            <div className="flex items-center justify-center  bg-yellow-500 w-full border rounded-full shadow-2xl px-4 py-2">
+              Get your ticket: tinyurl/devfest-mbarara-2025
+            </div>
+          </div>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 
