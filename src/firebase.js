066f293e5c4b98d5c1df8ec9cdf22c46f68ff270
Nahabwe Edwin <nahabwe.edwin12@gmail.com>
2025-10-31 14:10:13 +0300

    Convert firebase credentials to env

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,14 @@ import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, push, onValue } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC8b6RjUd3FXn62WMEUH0EPJwaWrwjTFhc",
-    authDomain: "devfestdp.firebaseapp.com",
-    projectId: "devfestdp",
-    storageBucket: "devfestdp.firebasestorage.app",
-    messagingSenderId: "1031650496039",
-    appId: "1:1031650496039:web:a3ba444f89edaa50fe0802",
-    measurementId: "G-H5STDW4TS5"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app)
